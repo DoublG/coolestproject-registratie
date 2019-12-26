062c0eb10b156d932cc5d8169eb11c78ce02b79c
Woidt <Erik.Woidt@ordina.be>
2019-12-26 18:44:19 +0100

    edit and delete project

--- pages/project.vue
+++ pages/project.vue
@@ -2,79 +2,144 @@
   <b-row>
     <b-col>
       <h1>{{ $t('title') }}</h1>
-      <ValidationObserver ref="observer" v-slot="{ passes }">
-         <b-form @submit.prevent="passes(onSubmit)" @reset.prevent="onReset">
-          <ValidationProvider v-slot="{ valid, errors }" rules="required" name="ProjectName">
-            <b-form-group
-              id="input-group-2"
-              :label="$t('ProjectName:')"
-              label-for="input-2"
-            >
-              <b-form-input
-                id="input-2"
-                v-model="project_name"
-                :placeholder="$t('GeefProjectName:')"
-                :state="errors[0] ? false : (valid ? true : null)"
-                aria-describedby="input-2-live-feedback"
-              />
-              <b-form-invalid-feedback id="input-2-live-feedback">
-                {{ errors[0] }}
-              </b-form-invalid-feedback>
+      <div v-if="hasProject || createState">
+        <ValidationObserver ref="observer" v-slot="{ passes }">
+          <b-form @submit.prevent="passes(onSubmit)" @reset.prevent="onReset">
+            <ValidationProvider v-slot="{ valid, errors }" rules="required" name="Language">
+              <b-form-group
+                id="input-group-18"
+                :label="$t('Taal:')"
+                label-for="select-18"
+                :description="$t('taalJury')"
+              >
+                <b-form-select
+                  id="select-18"
+                  v-model="project_lang"
+                  :options="languages"
+                  :state="errors[0] ? false : (valid ? true : null)"
+                  aria-describedby="input-18-live-feedback"
+                />
+                <b-form-invalid-feedback id="input-18-live-feedback">
+                  {{ errors[0] }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ valid, errors }" name="ProjectType">
+              <b-form-group
+                id="input-group-166"
+                :label="$t('Project_Type')"
+                label-for="input-166"
+              >
+                <b-form-textarea
+                  id="input-166"
+                  v-model="project_type"
+                  :state="errors[0] ? false : (valid ? true : null)"
+                  aria-describedby="input-166-live-feedback"
+                />
+                <b-form-invalid-feedback id="input-166-live-feedback">
+                  {{ errors[0] }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ valid, errors }" rules="required|max:100" name="ProjectName">
+              <b-form-group
+                id="input-group-20"
+                :label="$t('Projectnaam:')"
+                label-for="input-20"
+              >
+                <b-form-input
+                  id="input-20"
+                  v-model="project_name"
+                  :placeholder="$t('GeefProjectnaam:')"
+                  :state="errors[0] ? false : (valid ? true : null)"
+                  aria-describedby="input-20-live-feedback"
+                />
+                <b-form-invalid-feedback id="input-20-live-feedback">
+                  {{ errors[0] }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ valid, errors }" rules="required|max:4000" name="ProjectDescription">
+              <b-form-group
+                id="input-group-21"
+                :label="$t('Omschrijving:')"
+                label-for="input-21"
+              >
+                <b-form-textarea
+                  id="input-21"
+                  v-model="project_descr"
+                  :state="errors[0] ? false : (valid ? true : null)"
+                  aria-describedby="input-21-live-feedback"
+                />
+                <b-form-invalid-feedback id="input-21-live-feedback">
+                  {{ errors[0] }}
+                </b-form-invalid-feedback>
+              </b-form-group>
+            </ValidationProvider>
+            <b-form-group>
+              <b-button
+                v-if="createState"
+                type="submit"
+                variant="info"
+                class="button-hero"
+              >
+                <font-awesome-icon :icon="['fas', 'user-edit']" /> {{ $t('Create') }}
+              </b-button>
+              <b-button
+                v-else
+                type="submit"
+                variant="info"
+                class="button-hero"
+              >
+                <font-awesome-icon :icon="['fas', 'user-edit']" /> {{ $t('Aanpassen') }}
+              </b-button>
+              <b-button
+                v-if="!createState"
+                type="reset"
+                variant="warning"
+                class="button-hero"
+              >
+                <font-awesome-icon :icon="['fas', 'trash-restore']" />  {{ $t('Resetten') }}
+              </b-button>
+              <b-button
+                v-if="!createState"
+                type="button"
+                variant="danger"
+                class="button-hero"
+                @click="onDeleteInfo"
+              >
+                <font-awesome-icon :icon="['fas', 'user-minus']" />  {{ $t('Delete') }}
+              </b-button>
+              <b-modal v-model="deleteInfo" @ok="onDelete" okTitle="Delete">
+                Project wordt gedelete
+              </b-modal>
             </b-form-group>
-          </ValidationProvider>
-          <ValidationProvider v-slot="{ valid, errors }" rules="required" name="ProjectDescr">
-            <b-form-group
-              id="input-group-1"
-              :label="$t('ProjectDescr:')"
-              label-for="input-1"
-            >
-              <b-form-input
-                id="input-1"
-                v-model="project_descr"
-                :placeholder="$t('GeefProjectDescr:')"
-                :state="errors[0] ? false : (valid ? true : null)"
-                aria-describedby="input-1-live-feedback"
-              />
-              <b-form-invalid-feedback id="input-1-live-feedback">
-                {{ errors[0] }}
-              </b-form-invalid-feedback>
-            </b-form-group>
-          </ValidationProvider>
-          <b-form-group>
-            <b-button
-              type="submit"
-              variant="info"
-              class="button-hero"
-            >
-              <font-awesome-icon :icon="['fas', 'user-edit']" /> {{ $t('Aanpassen') }}
-            </b-button>
-            <b-button
-              type="reset"
-              variant="warning"
-              class="button-hero"
-            >
-              <font-awesome-icon :icon="['fas', 'trash-restore']" />  {{ $t('Resetten') }}
-            </b-button>
-            <b-button
-              type="button"
-              variant="danger"
-              class="button-hero"
-              @click="onDeleteInfo"
-            >
-              <font-awesome-icon :icon="['fas', 'user-minus']" />  {{ $t('Delete') }}
-            </b-button>
-            <b-modal v-model="deleteInfo" @ok="onDelete" okTitle="Delete">
-              Project wordt gedelete
-            </b-modal>
-          </b-form-group>
-         </b-form>
-      </ValidationObserver>
+          </b-form>
+        </ValidationObserver>
+      </div>
+      <div v-else>
+        <b-button
+          type="button"
+          variant="success"
+          class="button-hero"
+          @click="onCreateProject"
+        >
+          <font-awesome-icon :icon="['fas', 'project-diagram']" />  {{ $t('CreateProject') }}
+        </b-button>
+        <b-button
+          type="button"
+          variant="success"
+          class="button-hero"
+          @click="onEnterToken"
+        >
+          <font-awesome-icon :icon="['fas', 'user-minus']" />  {{ $t('EnterToken') }}
+        </b-button>
+      </div>
     </b-col>
   </b-row>
 </template>
 <script>
 import axios from 'axios'
-import { mapState } from 'vuex'
 import { ValidationObserver, ValidationProvider } from 'vee-validate'
 
 export default {
@@ -85,7 +150,13 @@ export default {
   },
   data () {
     return {
-      deleteInfo: false
+      deleteInfo: false,
+      createState: false,
+      languages: [
+        { value: 'nl', text: 'Nederlands' },
+        { value: 'fr', text: 'Frans' },
+        { value: 'en', text: 'Engels' }
+      ]
     }
   },
   computed: {
@@ -105,20 +176,45 @@ export default {
         return this.$store.state.project.project_descr
       }
     },
-    ...mapState('project', [
-      'project_type',
-      'project_lang'
-    ])
+    project_type: {
+      set (value) {
+        this.$store.commit('project/project_type', value)
+      },
+      get () {
+        return this.$store.state.project.project_type
+      }
+    },
+    project_lang: {
+      set (value) {
+        this.$store.commit('project/project_lang', value)
+      },
+      get () {
+        return this.$store.state.project.project_lang
+      }
+    },
+    hasProject () {
+      return this.$store.state.project.project_id !== null
+    }
   },
   async asyncData ({ store, query, app, redirect, route }) {
     // load projectData & store in userstore
-    const projectData = await axios.get('/api/projectinfo', { headers: { api_key: store.state.auth.api_key } })
-    await store.dispatch('project/updateProject', projectData.data)
+    try {
+      const projectData = await axios.get('/api/projectinfo', { headers: { api_key: store.state.auth.api_key } })
+      if (projectData.data !== '') {
+        await store.dispatch('project/updateProject', projectData.data)
+      }
+    } catch (error) {}
   },
   methods: {
     async onSubmit (evt) {
       try {
-        const projectData = await this.$axios.$patch('/api/projectinfo', this.$store.getters['project/projectinfo'], { headers: { api_key: this.$store.state.auth.api_key } })
+        let projectData = null
+        if (this.createState) {
+          projectData = await this.$axios.$post('/api/projectinfo', this.$store.getters['project/projectinfo'], { headers: { api_key: this.$store.state.auth.api_key } })
+          this.createState = false
+        } else {
+          projectData = await this.$axios.$patch('/api/projectinfo', this.$store.getters['project/projectinfo'], { headers: { api_key: this.$store.state.auth.api_key } })
+        }
         await this.$store.dispatch('project/updateProject', projectData)
       } catch (error) {
         this.variant = 'danger'
@@ -136,7 +232,13 @@ export default {
     },
     async onDelete (evt) {
       await this.$axios.$delete('/api/projectinfo', { headers: { api_key: this.$store.state.auth.api_key } })
-    }
+      await this.$store.dispatch('project/updateProject', null)
+    },
+    onCreateProject (evt) {
+      this.project_lang = this.$i18n.locale
+      this.createState = true
+    },
+    onEnterToken (evt) {}
   }
 }
 </script>
--- store/project.js
+++ store/project.js
@@ -1,18 +1,24 @@
 export const state = () => ({
-  project_name: '',
-  project_descr: '',
-  project_type: '',
-  project_lang: ''
+  project_id: null,
+  project_name: null,
+  project_descr: null,
+  project_type: null,
+  project_lang: null
 })
 export const getters = {
   projectinfo: (state) => {
     return {
       project_name: state.project_name,
-      project_descr: state.project_descr
+      project_descr: state.project_descr,
+      project_type: state.project_type,
+      project_lang: state.project_lang
     }
   }
 }
 export const mutations = {
+  project_id (state, projectId) {
+    state.project_id = projectId
+  },
   project_name (state, projectName) {
     state.project_name = projectName
   },
@@ -29,9 +35,18 @@ export const mutations = {
 
 export const actions = {
   updateProject ({ commit }, project) {
-    commit('project_name', project.project_name)
-    commit('project_descr', project.project_descr)
-    commit('project_type', project.project_type)
-    commit('project_lang', project.project_lang)
+    if (project !== null) {
+      commit('project_id', project.project_id)
+      commit('project_name', project.project_name)
+      commit('project_descr', project.project_descr)
+      commit('project_type', project.project_type)
+      commit('project_lang', project.project_lang)
+    } else {
+      commit('project_id', null)
+      commit('project_name', null)
+      commit('project_descr', null)
+      commit('project_type', null)
+      commit('project_lang', null)
+    }
   }
 }
