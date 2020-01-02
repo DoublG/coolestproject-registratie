856f05de931f8df251c9100dae582b671eb4df65
Woidt <Erik.Woidt@ordina.be>
2020-01-02 23:40:52 +0100

    extra check op undefined for no_project redirect

--- middleware/project.js
+++ middleware/project.js
@@ -4,13 +4,13 @@ export default async function ({ store, redirect }) {
   // load projectData
   try {
     const projectData = await axios.get('/api/projectinfo', { headers: { api_key: store.state.auth.api_key } })
-    if (projectData.data !== '') {
+    if (projectData.data !== null) {
       await store.dispatch('project/updateProject', projectData.data)
     }
   } catch (error) {}
 
   // no project found
-  if (store.state.project.project_name === null) {
+  if (store.state.project.project_name === null || store.state.project.project_name === undefined) {
     redirect('./no_project')
   }
 }
