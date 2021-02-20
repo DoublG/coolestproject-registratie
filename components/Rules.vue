1b26f3afd46592f7970485769c1205af192578a2
Erik Woidt <erik@woidt.be>
2021-02-20 13:02:24 +0100

    rules page

---
+++ components/Rules.vue
@@ -0,0 +1,14 @@
+<template>
+  <ol>
+    <li v-html="$t('rule0')" />
+    <li v-html="$t('rule1')" />
+    <li v-html="$t('rule2')" />
+    <li v-html="$t('rule3')" />
+    <li v-html="$t('rule4')" />
+    <li v-html="$t('rule5')" />
+    <li v-html="$t('rule6')" />
+    <li v-html="$t('rule7')" />
+    <li v-html="$t('rule8')" />
+    <li v-html="$t('rule9')" />
+  </ol>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,20 +9,8 @@
         <b> {{ $t('agree') }} </b>
       </p>
       <p>
-        <ol>
-          <li v-html="$t('rule0')" />
-          <li v-html="$t('rule1')" />
-          <li v-html="$t('rule2')" />
-          <li v-html="$t('rule3')" />
-          <li v-html="$t('rule4')" />
-          <li v-html="$t('rule5')" />
-          <li v-html="$t('rule6')" />
-          <li v-html="$t('rule7')" />
-          <li v-html="$t('rule8')" />
-          <li v-html="$t('rule9')" />
-        </ol>
-        <b> {{ $t('problems') }}
-        </b>
+        <rules />
+        <b> {{ $t('problems') }} </b>
         <span v-html="$t('href')" />
       </p>
       <b-alert v-if="noEvent" show variant="warning">
@@ -37,8 +25,6 @@
 <script>
 export default {
   layout: 'fullwith',
-  components: {
-  },
   async asyncData ({ store, query, app, redirect, route }) {
     const settings = await app.$services.settings.get()
     if (!settings) {
