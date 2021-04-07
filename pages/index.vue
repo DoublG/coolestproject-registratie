621cd5ce7486077510ac66f34779c72c1fc7b7b9
Erik Woidt <erik@woidt.be>
2021-04-07 09:16:23 +0200

    simplify event active conditions

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,13 @@
         <b> {{ $t('problems') }} </b>
         <span v-html="$t('href')" />
       </p>
-      <b-alert v-if="noEvent" show variant="warning">
+      <b-alert v-if="!settings" show variant="warning">
         {{ $t('No Event is active please come again later') }}
       </b-alert>
-      <b-alert v-if="!noEvent && settings.waitingListActive" show variant="warning">
+      <b-alert v-if="settings && settings.waitingListActive" show variant="warning">
         {{ $t('Waiting list is active') }}
       </b-alert>
-      <b-button v-if="!noEvent" variant="dark" class="button-hero" :to="localePath('registration')">
+      <b-button v-if="settings && settings.isActive" variant="dark" class="button-hero" :to="localePath('registration')">
         <font-awesome-icon :icon="['fas', 'paper-plane']" /> {{ $t('start') }}
       </b-button>
     </b-container>
@@ -30,15 +30,10 @@ export default {
   layout: 'fullwith',
   async asyncData ({ store, query, app, redirect, route }) {
     const settings = await app.$services.settings.get()
-    if (!settings) {
-      return { noEvent: true }
-    }
     return { settings }
   },
   data () {
-    return {
-      noEvent: false
-    }
+    return {}
   }
 }
 </script>
