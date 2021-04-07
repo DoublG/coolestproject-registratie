b424570cb6cccb93beeec48e107fd84b825466a8
Erik Woidt <erik@woidt.be>
2021-04-07 10:02:30 +0200

    cleanup UI

--- pages/index.vue
+++ pages/index.vue
@@ -13,10 +13,10 @@
         <b> {{ $t('problems') }} </b>
         <span v-html="$t('href')" />
       </p>
-      <b-alert v-if="!settings" show variant="warning">
+      <b-alert v-if="!settings || !settings.isActive" show variant="warning">
         {{ $t('No Event is active please come again later') }}
       </b-alert>
-      <b-alert v-if="settings && settings.waitingListActive" show variant="warning">
+      <b-alert v-if="settings && settings.isActive && settings.waitingListActive" show variant="warning">
         {{ $t('Waiting list is active') }}
       </b-alert>
       <b-button v-if="settings && settings.isActive" variant="dark" class="button-hero" :to="localePath('registration')">
