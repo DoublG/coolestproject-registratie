ead8f11d8dda401d5f227d66fd20d37bc4107b88
Erik Woidt <erik@woidt.be>
2021-04-08 15:32:45 +0200

    version bumb

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   dotenv: {
   },
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Coolestproject',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
