353444aa8078c84f43149b27bc7412d7703d09b4
Olivier Hubaut <olivier@hubaut.info>
2022-01-25 23:14:02 +0100

    Expose the bus at both the global and context level

--- plugins/bus.ts
+++ plugins/bus.ts
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+import { Plugin } from '@nuxt/types'
 
 declare module 'vue/types/vue' {
   interface Vue {
     $bus: Vue
   }
 }
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $bus: Vue
+  }
+}
 
-Vue.prototype.$bus = new Vue()
+const bus: Plugin = (_, inject) => {
+  inject('bus', new Vue())
+}
+
+export default bus
